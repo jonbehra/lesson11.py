7657f48796f65bbfde51998f47a874a84c2cee35
jonbehra <165320382+jonbehra@users.noreply.github.com>
2025-05-08 20:50:32 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 file_path = "C:/Users/Student/Desktop/lesson11.py/leximi.txt"
-file = open(file_path, "r")
+# file = open(file_path, "r")
+# with open (file_path, "r") as file: 
+#     line1 = file.readline()
+#     print(line1)
 
-content = file.read()
-print(content)
+# with open (file_path, "w") as file: 
+#     file.write("Hello world")
+
+with open(file_path, "w")as file:
+    file.seek(20)
+    line = file.read()
+    print(line)
+    
